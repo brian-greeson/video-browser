6c6a1a78be9c135c17838895e089d385c70d550a
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-09-11 09:08:04 -0600

    Add env

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <SearchBar @searchChange="search"></SearchBar>
+    <SearchBar @searchChange="onSearchChange"></SearchBar>
   </div>
 </template>
 
 <script>
 import SearchBar from "./components/SearchBar";
+
 export default {
   name: "App",
+  data() {
+    return {
+      apiKey: process.env.VUE_APP_YOUTUBE_KEY,
+    };
+  },
   components: {
     SearchBar,
   },
   methods: {
-    search: function (query) {
+    onSearchChange(query) {
       console.log("got a new search: " + query);
     },
   },
