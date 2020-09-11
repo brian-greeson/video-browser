2efd339aa5905cd0af27d8b66c1ce94bd99c092d
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-09-11 09:58:59 -0600

    Add call to youtube api

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,41 @@
 <template>
   <div>
     <SearchBar @searchChange="onSearchChange"></SearchBar>
+    <VideoList></VideoList>
   </div>
 </template>
 
 <script>
 import SearchBar from "./components/SearchBar";
-
+import VideoList from "./components/VideoList";
+import axios from "axios";
+const YOUTUBE_KEY = process.env.VUE_APP_YOUTUBE_KEY;
 export default {
   name: "App",
-  data() {
-    return {
-      apiKey: process.env.VUE_APP_YOUTUBE_KEY,
-    };
-  },
   components: {
     SearchBar,
+    VideoList,
+  },
+  data: function () {
+    return {
+      searchResults: {},
+    };
   },
   methods: {
     onSearchChange(query) {
-      console.log("got a new search: " + query);
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search/", {
+          params: {
+            key: YOUTUBE_KEY,
+            type: "video",
+            maxResults: 10,
+            part: "snippet",
+            q: query,
+          },
+        })
+        .then((response) => {
+          this.searchResults = response.data.items;
+        });
     },
   },
 };
---
+++ src/components/VideoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    Video List:
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "VideoList",
+};
+</script>
+
+<style>
+</style>
