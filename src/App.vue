46a79a033d78de2b71cdba2ed39bae76e2a813c8
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-09-10 18:39:44 -0600

    Add search bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SearchBar @searchChange="search"></SearchBar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SearchBar from "./components/SearchBar";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SearchBar,
+  },
+  methods: {
+    search: function (query) {
+      console.log("got a new search: " + query);
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: function (event) {
+      this.$emit("searchChange", event.target.value);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
